73ccf790631511bff90b9706dca525b4b10c3030
IuliiaKot <kotlenko.julia@gmail.com>
2017-03-29 12:01:43 -0400

    add create strory action

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -4,8 +4,27 @@ class StoriesController < ApplicationController
     @stories = Story.all
   end
 
+  def new
+    @story =  Story.new
+  end
+
+  def create
+    @story = Story.new(story_params)
+    if @story.save
+      redirect_to story_path(@story), notice: "Story was successfully posted"
+    else
+      @errors = @story.errors.full_messages
+      render :new
+    end
+  end
+
   def show
     @story = Story.find_by(id: params[:id])
   end
+
+  private
+    def story_params
+      params.require(:story).permit(:title, :content)
+    end
 
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,3 +1,5 @@
 class Story < ApplicationRecord
+  validates :title, :content, presence: true
+
   has_many :comments, as: :commentable
 end
